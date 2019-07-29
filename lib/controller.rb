744058ba0d99735b55ef32223d8285f63ac48fd9
iroshiva <jean.weil@protonmail.com>
2019-07-29 20:40:56 +0200

    commentaires

--- lib/controller.rb
+++ lib/controller.rb
@@ -10,10 +10,10 @@ class ApplicationController < Sinatra::Base
 
   	erb :index, locals: {gossips: Gossip.all}
   	# "si quelqu'un va sur '/', affiche la view index.erb"
-  	# locals: == permet d'envoyer à notre fichier ERB des variables que l'on utilisera.
+  	# locals: == permet d'envoyer à notre fichier erb corecpondant des variables que l'on utilisera.
   	# ici, veut dire gossips = Gossip.all
 
-	end
+  end
 	# Cela dit à l'appli Sinatra "si quelqu'un va sur l'URL '/', exécute le code qui suit !"
 
 	get '/gossips/new/' do
@@ -24,17 +24,24 @@ class ApplicationController < Sinatra::Base
 	post '/gossips/new/' do
 	# post == plutôt une action à faire
 
-   Gossip.new(params["gossip_author"], params["gossip_content"]).save
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save
+    # method qui prend l'attribut des names dans le view new_gossip.erb
+    # crée une instance initialisée par deux variables author("gossip_authur") et content("gossip_content") détaillée dans gossip.rb
 
-   redirect '/'
+    redirect '/'
+    # après la création du potin, redirection vers la page d'accueil
+
 	end
 
 	get '/gossips/:id' do
 		erb :show, locals: {gossip: Gossip.find(params[:id]), id: params[:id]}
+		# deux variables sont injectées dans show.erb: gossip = method self.find de la class Gossip et id = variable de la methode self.find
+
 	end
 
 	get '/gossips/:id/edit' do
 		erb :edit, locals: {id: params[:id]}
+		# variable id injectée dans edit.erb
 	end
 
 	post '/gossips/:id/edit' do
@@ -42,6 +49,7 @@ class ApplicationController < Sinatra::Base
 
    redirect '/'
 	end
+	# idem que pour le post '/gossips/new/' do
 
 end
 
